b9a9775264c63d44888e07f9b7371ab117c4bd01
Benjamin Lee <benjamilee@deloitte.com.au>
2019-06-12 17:00:07 +1000

    Add Facebook demo.

--- cypress/integration/facebook.feature
+++ cypress/integration/facebook.feature
@@ -2,5 +2,8 @@ Feature: The Facebook Page
 I want to open a social network Page
 
 Scenario: Opening a social network page
-    Given I open Facebook page
-    Then I see "Facebook" in the title
+    Given I open the url "https://www.facebook.com"
+    Then  I expect the url to contain "facebook.com"
+    And   I expect that the title is "Facebook – log in or sign up"
+    And   I expect that the title  contains "Facebook"
+
--- cypress/support/step_definitions/facebook.js
+++ cypress/support/step_definitions/facebook.js
@@ -1,10 +1,24 @@
-/* global Given, When, Then */
-const url = 'https://www.facebok.com';
+// Based on
+// https://github.com/monch1962/cucumber-boilerplate.git
+// src/steps/given.js
+// src/steps/then.js
 
-Given('I open Facebook page', () => {
+Given(/I open the (url|site) "([^"]*)?"$/, (garbage, url) => {
     cy.visit(url);
 });
 
-Then('I see {string} in the title', (title) => {
-    cy.title().should('include', title)
+Then(
+    /^I expect that the title ( not)* contains "([^"]*)?"$/, (not, title) => {
+    cy.title().should('include', title);
 });
+
+Then(
+    /^I expect that the title is( not)* "([^"]*)?"$/, (not, title) => {
+    cy.title().should('equal', title);
+});
+
+Then(
+    /^I expect the url to( not)* contain "([^"]*)?"$/, (not, urlText) => {
+    cy.url().should('include', urlText);
+});
+
